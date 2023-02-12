1acfb5e72015f9d9ca9abcce01ec53b0650d4cb1
Myles2005 <cyrile450@gmail.com>
2023-02-12 13:22:43 -0800

    created file storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-" "
+"Module for FileStorage class"
 
+import os
 import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
@@ -12,13 +13,14 @@ from models.state import State
 from models.user import User
 
 class FileStorage:
-    """
+    """Representation of FileStorage
+    Attributes:
+        __file_path: The name of the file to save objects to.
+        __objects: A dictionary of instantiated objects.
     """
     __file_path = 'data.json'
     __objects = dict()
 
-    classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City, \
-            "Place": Place, "Review": Review, "State": State, "User": User}
     def all(self):
         """Returns the dictionary __objects"""
         return FileStorage.__objects
@@ -32,16 +34,11 @@ class FileStorage:
         """ Serialization of __object to Json file """
         odict = FileStorage.__objects.copy()
         with open(FileStorage.__file_path, 'w') as f:
-            for k, v in odict.items():
-                data = {k: v.to_dict()}
+            data = {k: v.to_dict() for k, v in odict.items()}
             json.dump(data, f)
 
     def reload(self):
         """ Deserialization of Json file to __objects """
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                data = json.load(f)
-                for k, v in data.items():
-                    FileStorage.__object[k] = classes[v["__class__"]](**v)
-        except FileNotFoundError:
-            pass
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as f:
+                d = json.load(f)
